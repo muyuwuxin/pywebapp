d816416fe13854f7f19f8f94043f1f9cccb92dbd
TBCCC <tbccc55555@gmail.com>
2017-03-01 16:55:35 +0800

    add todo successfully

--- app/todo/forms.py
+++ app/todo/forms.py
@@ -1,2 +1,38 @@
 #! /usr/bin/env python2
 # -*-coding:utf-8-*-
+
+
+from flask_wtf import FlaskForm
+from wtforms import RadioField, SubmitField, StringField, PasswordField, ValidationError
+from wtforms.validators import DataRequired, Length, EqualTo
+from ..models import User
+
+
+class TodoListForm(FlaskForm):
+    title = StringField(u'Title', validators=[DataRequired(), Length(1, 64)])
+    status = RadioField(u'Finished or not', validators=[DataRequired()],  choices=[
+                        ("1", u'Finished'), ("0", u'Unfinished')])
+    submit = SubmitField(u'Submit')
+
+
+# class LoginForm(FlaskForm):
+#     username = StringField(u'Username', validators=[
+#                            DataRequired(), Length(1, 24)])
+#     password = PasswordField(
+#         u'Password', validators=[DataRequired(), Length(1, 24)])
+#     submit = SubmitField(u'Sign in')
+
+
+# class RegisterForm(FlaskForm):
+#     username = StringField(u'Username', validators=[
+#                            DataRequired(), Length(1, 24)])
+#     password = PasswordField(
+#         u'Password', validators=[DataRequired(), EqualTo('password2', message=u'password wrong1')])
+#     password2 = PasswordField(u'Repeat the password',
+#                               validators=[DataRequired()])
+#     submit = SubmitField(u'Sign up')
+#
+#     def validate_username(self, field):  # 解决重复的用户名而加上
+#         if User.query.filter_by(username=field.data).first():
+#             raise ValidationError(u'The username has existed!')
+#         pass
--- app/todo/views.py
+++ app/todo/views.py
@@ -1,2 +1,127 @@
 #! /usr/bin/env python2
 # -*-coding:utf-8-*-
+
+
+from . import todo
+from .. import db, login_manager
+from ..models import TodoList, User
+from flask_login import login_required, login_user, logout_user, current_user
+# from .forms import TodoListForm, LoginForm, RegisterForm
+from .forms import TodoListForm
+from flask import render_template, redirect, request, url_for, flash
+
+
+@todo.route('/', methods=['GET', 'POST'])
+@login_required
+def show_todo_list():
+    form = TodoListForm()
+    if request.method == 'GET':
+        todolists = TodoList.query.filter_by(
+            user_id=current_user.id).all()
+        return render_template('todo/index.html', todolists=todolists, form=form, username=current_user.username)
+    else:
+        if form.validate_on_submit():
+            todolist = TodoList(
+                user_id=current_user.id, title=form.title.data, status=form.status.data)
+            try:
+                db.session.add(todolist)
+                db.session.commit()
+                flash(u'You have added a todo sucessfully')
+            except:
+                db.session.rollback()
+                raise
+        else:
+            flash(u'Please log in first')
+        return redirect(url_for('todo.show_todo_list'))
+
+
+@todo.route('/delete/<int:id>')
+@login_required
+def delete_todo_list(id):
+    todolist = TodoList.query.filter_by(id=id).first_or_404()
+    try:
+        db.session.delete(todolist)
+        db.session.commit()
+        flash(u'You delete a todo sucessfully')
+        return redirect(url_for('todo.show_todo_list'))
+    except:
+        db.session.rollback()
+        raise
+
+
+@todo.route('/change/<int:id>', methods=['GET', 'POST'])
+@login_required
+def change_todo_list(id):
+    if request.method == 'GET':
+        todolist = TodoList.query.filter_by(id=id).first_or_404()
+        form = TodoListForm()
+        form.title.data = todolist.title
+        form.status.data = str(todolist.status)
+        return render_template('todo/modify.html', form=form)
+    else:
+        form = TodoListForm()
+        if form.validate_on_submit():
+            todolist = TodoList.query.filter_by(id=id).first_or_404()
+            todolist.title = form.title.data
+            todolist.status = form.status.data
+            try:
+                db.session.commit()
+                flash(u'You modify a todo sucessfully')
+            except:
+                db.session.rollback()
+        else:
+            flash(form.errors)
+        return redirect(url_for('todo.show_todo_list'))
+
+
+# @todo.route('/register', methods=['GET', 'POST'])
+# def register():
+#     form = RegisterForm()
+#     if request.method == 'GET':
+#         return render_template('register.html', form=form)
+#     if form.validate_on_submit:
+#         if User.query.filter_by(username=form.username.data).first():
+#             flash("The username has existed")
+#             return redirect(url_for('todo.register'))
+#         else:
+#             if form.password.data != form.password2.data:
+#                 flash(u'The two password is different')
+#                 return redirect(url_for('todo.register'))
+#             else:
+#                 user = User(username=form.username.data,
+#                             password=form.password.data)
+#                 try:
+#                     db.session.add(user)
+#                     flash(u'You have signed up a account sucessfully')
+#                     return redirect(url_for('todo.login'))
+#                 except:
+#                     db.session.rollback()
+#                     raise
+
+
+# @todo.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         user = User.query.filter_by(
+#             username=request.form['username'], password=request.form['password']).first()
+#         if user:
+#             login_user(user)
+#             flash('You have logged in!')
+#             return redirect(url_for('todo.show_todo_list'))
+#         else:
+#             flash(u'Invalid name or password')
+#     form = LoginForm()
+#     return render_template('login.html', form=form)
+
+
+# @todo.route('/logout')
+# @login_required
+# def logout():
+#     logout_user()
+#     flash(u'You have signed out sucessfully')
+#     return redirect(url_for('todo.login'))
+
+
+# @login_manager.user_loader
+# def load_user(user_id):
+#     return User.query.filter_by(id=int(user_id)).first()
